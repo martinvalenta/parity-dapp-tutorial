8dbfc0767aa9fde64d3f3141fac06c02fad37c52
Martin Valenta <martin.valenta@gmx.net>
2017-04-25 03:45:54 +0200

    7th Part finished

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,5 +1,5 @@
 import {Bond, TimeBond} from 'oo7';
-import {TextBond, Rspan, Hash, HashBond, Rimg, RRaisedButton} from 'oo7-react';
+import {TextBond, Rspan, Hash, HashBond, Rimg, RRaisedButton, ReactiveComponent} from 'oo7-react';
 import {formatBlockNumber, formatBalance, isNullData} from 'oo7-parity';
 import {TransactionProgressBadge} from 'parity-reactive-ui';
 import React from 'react';
@@ -16,27 +16,40 @@ export class App extends React.Component {
 		super();
 		this.counter = parity.bonds.makeContract('0x83d85eEB38A2dC37EAc0239c19b343a7653d8F79', CounterABI);
 		this.state = { tx: null };
+		this.voted = this.counter.hasVoted(parity.bonds.me);
 	}
 	render () {
+		var votingEnabled = Bond.all([this.voted, this.state.tx]).map(([v, t]) => !v && (!t || !!t.failed));
 		return (<div>
-			{Options.map((n, i) => (<div key={i}>
-				<Rspan style={{
-					borderRight: this.counter
-						.votes(i)
-						.map(v => `${1 + v * 10}px black solid`)
-				}}>
-					<a
-						style={{float: 'left', minWidth: '3em'}}
-						href='#'
-						onClick={() => this.setState({tx: this.counter.vote(i)})}
-					>
-						{n}
-					</a>
-				</Rspan>
-			</div>))}
+			{Options.map((n, i) => (<div key={i}><VoteOption
+				label={n}
+				votes={this.counter.votes(i)}
+				vote={() => this.setState({tx: this.counter.vote(i)})}
+				enabled={votingEnabled}
+			/></div>))}
 			<div style={{marginTop: '1em'}}>
 				<TransactionProgressBadge value={this.state.tx}/>
 			</div>
 		</div>);
+	}
+}
+
+class VoteOption extends ReactiveComponent {
+	constructor () {
+		super(['votes', 'enabled']);
+	}
+	readyRender () {
+		var s = {float: 'left', minWidth: '3em'};
+		if (!this.state.enabled)
+			s.cursor = 'not-allowed';
+		return (<span style={{ borderRight:
+			`${1 + this.state.votes * 10}px black solid` }}>
+			<a
+				style={s}
+				href='#'
+				onClick={this.state.enabled && this.props.vote}>
+				{this.props.label}
+			</a>
+		</span>);
 	}
 }
