fb2bdfe00ba1931de64e2318e8cdcb8d0e5a6eb2
Martin Valenta <martin.valenta@gmx.net>
2017-04-25 02:26:30 +0200

    5th Part finished

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,6 +1,6 @@
 import {Bond, TimeBond} from 'oo7';
-import {TextBond, Rspan, Hash, HashBond, Rimg} from 'oo7-react';
-import {formatBlockNumber, formatBalance} from 'oo7-parity';
+import {TextBond, Rspan, Hash, HashBond, Rimg, RRaisedButton} from 'oo7-react';
+import {formatBlockNumber, formatBalance, isNullData} from 'oo7-parity';
 import React from 'react';
 import styles from "../style.css";
 
@@ -10,15 +10,33 @@ const format = ([msg, t]) => `${new Date(t)}: ${msg}`
 export class App extends React.Component {
 	constructor() {
 		super();
-		this.bond = new Bond;
-		this.GithubHint = parity.bonds.makeContract(parity.bonds.registry.lookupAddress('githubhint', 'A'), parity.api.abi.githubhint);
+		this.name = new Bond;
+		this.recipient = parity.bonds.registry.lookupAddress(this.name, 'A');
+		this.state = { current: null };
 	}
 	render() {
 		return (
 			<div>
-				<TextBond bond={this.bond} floatingLabelText='Name' />
-				<Rimg src={this.GithubHint.entries(parity.bonds.registry.lookupData(this.bond, 'IMG'))[0]} />
+				My balance: <Rspan>
+					{parity.bonds.balance(parity.bonds.me).map(formatBalance)}
+				</Rspan>
+				<br />
+				<TextBond bond={this.name} floatingLabelText='Name of recipient' />
+				<RRaisedButton
+					label={this.name.map(n => `Give ${n} 100 Finney`)}
+					disabled={this.recipient.map(isNullData)}
+					onClick={this.give.bind(this)}
+				/>
+				<Rspan>{this.state.current && this.state.current.map(JSON.stringify)}</Rspan>
 			</div>
 		);
+	}
+	give () {
+	this.setState({
+		current: parity.bonds.post({
+			to: this.recipient,
+			value: 100 * 1e15
+			})
+		})
 	}
 }
