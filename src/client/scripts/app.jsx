671c5c9aab3ac9c7c8cfb100ecc0ab6cd1ccf78f
Martin Valenta <martin.valenta@gmx.net>
2017-05-12 17:49:47 +0200

    9th Part finished

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -8,21 +8,33 @@ import styles from "../style.css";
 const computeColor = t => t.match(/^[0-9]+$/) ? {color: 'red'} : {color: 'black'}
 const format = ([msg, t]) => `${new Date(t)}: ${msg}`
 
-const CounterCode = '0x6060604052341561000c57fe5b5b6102748061001c6000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680630121b93f1461005157806309eef43e146100715780635df81330146100bf575bfe5b341561005957fe5b61006f60048080359060200190919050506100f3565b005b341561007957fe5b6100a5600480803573ffffffffffffffffffffffffffffffffffffffff16906020019091905050610210565b604051808215151515815260200191505060405180910390f35b34156100c757fe5b6100dd6004808035906020019091905050610230565b6040518082815260200191505060405180910390f35b600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002060009054906101000a900460ff161561014a57610000565b60006000828152602001908152602001600020600081548092919060010191905055506001600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002060006101000a81548160ff021916908315150217905550803373ffffffffffffffffffffffffffffffffffffffff167f4d99b957a2bc29a30ebd96a7be8e68fe50a3c701db28a91436490b7d53870ca460405180905060405180910390a35b50565b60016020528060005260406000206000915054906101000a900460ff1681565b600060205280600052604060002060009150905054815600a165627a7a723058202cccc8011e9ec04f9987c54d8513d9f68a40e75c746013ea9cc7ff1e36f4ffb40029';
+const CounterCode = '0x6060604052341561000c57fe5b5b61017c8061001c6000396000f300606060405263ffffffff60e060020a6000350416630121b93f811461003757806309eef43e1461004c5780635df813301461007c575bfe5b341561003f57fe5b61004a6004356100a1565b005b341561005457fe5b610068600160a060020a0360043516610129565b604080519115158252519081900360200190f35b341561008457fe5b61008f60043561013e565b60408051918252519081900360200190f35b600160a060020a03331660009081526001602052604090205460ff16156100c757610000565b60008181526020818152604080832080546001908101909155600160a060020a03331680855292819052818420805460ff191690911790555183927f4d99b957a2bc29a30ebd96a7be8e68fe50a3c701db28a91436490b7d53870ca491a35b50565b60016020526000908152604090205460ff1681565b600060208190529081526040902054815600a165627a7a72305820c46de129e4ce714b358b0c0e1738e16a8f312832f34d1b6458a36902b3eab7b40029';
 const CounterABI = [{"constant":false,"inputs":[{"name":"_option","type":"uint256"}],"name":"vote","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"hasVoted","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"votes","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"who","type":"address"},{"indexed":true,"name":"option","type":"uint256"}],"name":"Voted","type":"event"}];
+const CounterCodeHash = '0xd2e1805684ab9cfe40edbe67669c7d4872ffcc69a3bb9a98f824853476b965f2';
 const Options = ['Red', 'Green', 'Blue'];
 
 export class App extends React.Component {
 	constructor () {
 		super();
-		this.state = { tx: null, counter: null };
+		this.addr = new Bond;
+		this.addr.then(v => {
+			window.localStorage.counter = v;
+			let counter = parity.bonds.makeContract(v, CounterABI);
+			this.state({ tx: null, counter});
+		});
+		this.state = { tx: null, counter: window.localStorage.counter
+			? parity.bonds.makeContract(window.localStorage.counter, CounterABI)
+			: null };
 		this.deploy = this.deploy.bind(this);
 	}
 
 	deploy () {
 		let tx = parity.bonds.deployContract(CounterCode, CounterABI);
 		this.setState({tx});
-		tx.done(s => this.setState({ counter: s.deployed }));
+		tx.done(s => {
+			this.setState({ tx: null, counter: s.deployed });
+			window.localStorage.counter = s.deployed.address;
+		});
 	}
 
 	render () {
@@ -31,6 +43,8 @@ export class App extends React.Component {
 					? <Counter contract={this.state.counter} />
 					: <div>
 						<RRaisedButton label='Deploy' onClick={this.deploy}/>
+						<span style={{margin: '2em'}}>OR</span>
+						<TextBond bond={this.addr} validator={v => v.startsWith('0x') && v.length == 42 && parity.bonds.code(v).map(_ => parity.api.util.sha3(_) == CounterCodeHash)}/>
 						<TransactionProgressBadge value={this.state.tx}/>
 					</div>
 				}
@@ -66,6 +80,9 @@ class Counter extends React.Component {
 			</Rspan>
 			<div style={{marginTop: '1em'}}>
 				<TransactionProgressBadge value={this.state.tx}/>
+			</div>
+			<div style={{fontSize: 'small'}}>
+				Using contract at {this.props.contract.address}.
 			</div>
 		</div>);
 	}
