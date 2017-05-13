f23d47d7042dbf4321bac9732c967fd39f71bc4d
Martin Valenta <martin.valenta@gmx.net>
2017-05-13 21:15:57 +0200

    10th Part finished

--- counter.sol
+++ counter.sol
@@ -3,12 +3,20 @@ pragma solidity ^0.4.0;
 contract Counter {
 	event Voted(address indexed who, uint indexed option);
 
-	function vote(uint _option) {
+	function vote(uint _option, uint8 _v, bytes32 _r, bytes32 _s)
+		only_signed(msg.sender, _v, _r, _s)
+	{
 		if (hasVoted[msg.sender]) throw;
 		votes[_option]++;
 		hasVoted[msg.sender] = true;
 		Voted(msg.sender, _option);
 	}
+
+	modifier only_signed(address who, uint8 v, bytes32 r, bytes32 s) { if (ecrecover(STATEMENT_HASH, v, r, s) == who) _; else throw; }
+
+	bytes32 constant public STATEMENT_HASH = sha3(STATEMENT);
+
+	string constant public STATEMENT = "\x19Ethereum Signed Message:\n22I am eligible to vote!";
 
 	mapping (uint => uint) public votes;
 	mapping (address => bool) public hasVoted;
--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,6 +1,6 @@
 import {Bond, TimeBond} from 'oo7';
 import {TextBond, Rspan, Hash, HashBond, Rimg, RRaisedButton, ReactiveComponent} from 'oo7-react';
-import {formatBlockNumber, formatBalance, isNullData} from 'oo7-parity';
+import {formatBlockNumber, formatBalance, isNullData, removeSigningPrefix} from 'oo7-parity';
 import {TransactionProgressBadge, AccountIcon} from 'parity-reactive-ui';
 import React from 'react';
 import styles from "../style.css";
@@ -8,9 +8,9 @@ import styles from "../style.css";
 const computeColor = t => t.match(/^[0-9]+$/) ? {color: 'red'} : {color: 'black'}
 const format = ([msg, t]) => `${new Date(t)}: ${msg}`
 
-const CounterCode = '0x6060604052341561000c57fe5b5b61017c8061001c6000396000f300606060405263ffffffff60e060020a6000350416630121b93f811461003757806309eef43e1461004c5780635df813301461007c575bfe5b341561003f57fe5b61004a6004356100a1565b005b341561005457fe5b610068600160a060020a0360043516610129565b604080519115158252519081900360200190f35b341561008457fe5b61008f60043561013e565b60408051918252519081900360200190f35b600160a060020a03331660009081526001602052604090205460ff16156100c757610000565b60008181526020818152604080832080546001908101909155600160a060020a03331680855292819052818420805460ff191690911790555183927f4d99b957a2bc29a30ebd96a7be8e68fe50a3c701db28a91436490b7d53870ca491a35b50565b60016020526000908152604090205460ff1681565b600060208190529081526040902054815600a165627a7a72305820c46de129e4ce714b358b0c0e1738e16a8f312832f34d1b6458a36902b3eab7b40029';
-const CounterABI = [{"constant":false,"inputs":[{"name":"_option","type":"uint256"}],"name":"vote","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"hasVoted","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"votes","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"who","type":"address"},{"indexed":true,"name":"option","type":"uint256"}],"name":"Voted","type":"event"}];
-const CounterCodeHash = '0xd2e1805684ab9cfe40edbe67669c7d4872ffcc69a3bb9a98f824853476b965f2';
+const CounterCode = '0x6060604052341561000c57fe5b5b6104a68061001c6000396000f300606060405263ffffffff60e060020a60003504166309eef43e811461004d5780635df813301461007d57806394c21ff3146100a2578063b876a628146100c4578063bebc479c146100e5575bfe5b341561005557fe5b610069600160a060020a0360043516610175565b604080519115158252519081900360200190f35b341561008557fe5b61009060043561018a565b60408051918252519081900360200190f35b34156100aa57fe5b61009061019c565b60408051918252519081900360200190f35b34156100cc57fe5b6100e360043560ff60243516604435606435610251565b005b34156100ed57fe5b6100f561041a565b60408051602080825283518183015283519192839290830191850190808383821561013b575b80518252602083111561013b57601f19909201916020918201910161011b565b505050905090810190601f1680156101675780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b60016020526000908152604090205460ff1681565b60006020819052908152604090205481565b6040805160608101825260328082527f19457468657265756d205369676e6564204d6573736167653a0a32324920616d602083019081527f20656c696769626c6520746f20766f7465210000000000000000000000000000838501529251919282918083835b602083106102215780518252601f199092019160209182019101610202565b6001836020036101000a038019825116818451168082178552505050505050905001915050604051809103902081565b3383838383600160a060020a03166001606060405190810160405280603281526020017f19457468657265756d205369676e6564204d6573736167653a0a32324920616d81526020017f20656c696769626c6520746f20766f74652100000000000000000000000000008152506040518082805190602001908083835b602083106102ed5780518252601f1990920191602091820191016102ce565b51815160209384036101000a60001901801990921691161790526040805192909401829003822060008381528383018652928501839052845190815260ff8c16818301528085018b9052606081018a9052935160808086019750919550601f19810194819003909101925090866161da5a03f1151561036857fe5b505060206040510351600160a060020a0316141561040a57600160a060020a03331660009081526001602052604090205460ff16156103a657610000565b60008881526020818152604080832080546001908101909155600160a060020a03331680855292819052818420805460ff19169091179055518a927f4d99b957a2bc29a30ebd96a7be8e68fe50a3c701db28a91436490b7d53870ca491a35b61040f565b610000565b5b5050505050505050565b606060405190810160405280603281526020017f19457468657265756d205369676e6564204d6573736167653a0a32324920616d81526020017f20656c696769626c6520746f20766f7465210000000000000000000000000000815250815600a165627a7a723058201354677f2c0f4a7806a59fe0e4240473df459fea985948db429c260ca1f68b9e0029';
+const CounterABI = [{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"hasVoted","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"votes","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"STATEMENT_HASH","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_option","type":"uint256"},{"name":"_v","type":"uint8"},{"name":"_r","type":"bytes32"},{"name":"_s","type":"bytes32"}],"name":"vote","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"STATEMENT","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"who","type":"address"},{"indexed":true,"name":"option","type":"uint256"}],"name":"Voted","type":"event"}];
+const CounterCodeHash = '0xbfeacbb0ea97a90427d902e3e976d6feda9a99419214bdd18c1f4b90a33aa854';
 const Options = ['Red', 'Green', 'Blue'];
 
 export class App extends React.Component {
@@ -56,6 +56,7 @@ class Counter extends React.Component {
 	constructor() {
 		super();
 		this.state = { tx: null, counter: null };
+		this.vote = this.vote.bind(this);
 	}
 	
 	componentWillMount () { this.componentWillReceiveProps(this.props); }
@@ -65,13 +66,23 @@ class Counter extends React.Component {
 	this.prevVotes = this.props.contract.Voted({ who: parity.bonds.accounts });
 	}
 
+	vote (option) {
+		let message = this.props.contract.STATEMENT().map(removeSigningPrefix);
+		console.log('vote');
+		parity.bonds.sign(message).done(s => {
+			if (s.signed) {
+				this.setState({tx: this.props.contract.vote(option, ...s.signed)});
+			}
+		})
+	}
+
 	render () {
 		var votingEnabled = Bond.all([this.voted, this.state.tx]).map(([v, t]) => !v && (!t || !!t.failed));
 		return (<div>
 			{Options.map((n, i) => (<div key={i}><VoteOption
 				label={n}
 				votes={this.props.contract.votes(i)}
-				vote={() => this.setState({tx: this.props.contract.vote(i)})}
+				vote={() => this.vote(i)}
 				enabled={votingEnabled}
 				already={this.prevVotes.map(a => a.filter(x => x.option == i).map(x => x.who))}
 			/></div>))}
